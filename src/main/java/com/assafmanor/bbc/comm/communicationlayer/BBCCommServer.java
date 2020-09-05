03f197a54366d22dbbd012b3a01ed54b30b234b4
assaf2017 <assafyaffe16@gmail.com>
2020-09-05 23:34:27 +0300

    Add log print

--- src/main/java/com/assafmanor/bbc/comm/communicationlayer/BBCCommServer.java
+++ src/main/java/com/assafmanor/bbc/comm/communicationlayer/BBCCommServer.java
@@ -76,7 +76,8 @@ public class BBCCommServer {
     }
 
     public ApproverMsg popApproveMsg(BBCMetaData meta, int r, int stage) {
-        LOGGER.log(Level.FINEST, "Start pop approve round "+r+" Meta Channel "+ meta.getChannel()+" Cid "+meta.getCid()+" CidSeries "+meta.getCidSeries());
+        LOGGER.log(Level.FINEST, "Start of approver debug...");
+        LOGGER.log(Level.FINEST, "Start pop approve round " + r + " Meta Channel " + meta.getChannel() + " Cid " + meta.getCid() + " CidSeries " + meta.getCidSeries());
         HashMap<Integer, HashMap<Integer, BlockingQueue<ApproveMsg>>> sessionMap;
         HashMap<Integer, BlockingQueue<ApproveMsg>> stageMap;
         BlockingQueue<ApproveMsg> msgQueue;
